92273cd11eeb4fca3041902cb4d6f13d4d6d68b3
Chris Lalancette <clalancette@openrobotics.org>
2021-03-29 18:30:34 +0000

    Rebase from 'debian/rolling/phidgets_spatial'

--- src/spatial_ros_i.cpp
+++ src/spatial_ros_i.cpp
@@ -279,8 +279,10 @@ void SpatialRosI::publishLatest()
 
     if (time_in_ns < last_ros_stamp_ns_)
     {
-        RCLCPP_WARN(get_logger(), "Time went backwards (%lu < %lu)!",
+        RCLCPP_WARN(get_logger(),
+                    "Time went backwards (%lu < %lu)! Not publishing message.",
                     time_in_ns, last_ros_stamp_ns_);
+        return;
     }
 
     last_ros_stamp_ns_ = time_in_ns;
@@ -399,7 +401,7 @@ void SpatialRosI::spatialDataCallback(const double acceleration[3],
             can_publish_ = true;
         } else
         {
-            RCLCPP_WARN(
+            RCLCPP_DEBUG(
                 get_logger(),
                 "Data not within acceptable window for synchronization: "
                 "expected between %ld and %ld, saw %ld",
